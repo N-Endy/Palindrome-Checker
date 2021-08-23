8f5b05c66c9bacc5121ea7d717aa83942c03f3f0
N-Endy <okafornel@gmail.com>
2021-08-23 12:48:44 +0100

    added check for if word input is empty

--- script.js
+++ script.js
@@ -3,27 +3,40 @@ let btn = document.querySelector('.btn');
 let result = document.querySelector('.result');
 
 btn.addEventListener('click', () => {
-    checkWord();
+    checkWord()
 })
 
 function checkWord() {
     word = document.querySelector('.input').value;
-    word = word.toLowerCase();
+    if (word === '') {
+        alert('Please input a word')
+    } else {
+        word = word.toLowerCase();
 
-    let word2 = [...word].filter((ele,idx) => word.charCodeAt(idx) > 96);
-    let result = word2.join('');
-    let final = word2.reverse().join('');
+        let word2 = [...word].filter((ele,idx) => word.charCodeAt(idx) > 96);
+        let result = word2.join('');
+        let final = word2.reverse().join('');
+        
+        
+        result === final ? isPalindrome() : isNotPalindrome();
+    }
     
-    
-    result === final ? isPalindrome() : isNotPalindrome();
 }
 
 function isPalindrome() {
     result.style.background = 'green'
     result.innerText = `The word '${word}' is a palindrome`;
+    setTimeout(function() {
+        result.innerText = '';
+        result.style.background = '#ffd5af'
+    }, 3000)
 }
 
 function isNotPalindrome() {
     result.style.background = 'red'
     result.innerText = `The word '${word}' is not a palindrome`;
+    setTimeout(function() {
+        result.innerText = '';
+        result.style.background = '#ffd5af'
+    }, 3000)
 }
